a16e27a67537b1e48593ad4afa81bba8ae7fd00b
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2024-07-14 14:01:35 +0600

    product management added

--- src/modules/order/order.interface.ts
+++ src/modules/order/order.interface.ts
@@ -1,26 +1,32 @@
 // order.model.ts
-interface IOrderItem {
-  productId: string;
-  productName: string;
-  quantity: number;
-  price: number;
-  total: number;
-}
+// interface IOrderItem {
+//   productId: string;
+//   productName: string;
+//   quantity: number;
+//   price: number;
+//   total: number;
+// }
 
-export interface IAddress {
-  street: string;
-  city: string;
-  state: string;
-  postalCode: string;
-  country: string;
-}
+// export interface IAddress {
+//   street: string;
+//   city: string;
+//   state: string;
+//   postalCode: string;
+//   country: string;
+// }
+
+// export interface IOrder {
+//   orderId: string;
+//   customarName: string;
+//   customarEmail: string;
+//   customarPhone: string;
+//   items: IOrderItem[];
+//   shippingAddress: IAddress;
+//   status: 'pending' | 'on-progress' | 'deleveried' | 'rejected' | 'returned';
+// }
 
 export interface IOrder {
-  orderId: string;
-  customarName: string;
-  customarEmail: string;
-  customarPhone: string;
-  items: IOrderItem[];
-  shippingAddress: IAddress;
-  status: 'pending' | 'on-progress' | 'deleveried' | 'rejected' | 'returned';
+  name:string;
+  email:string;
+  phone:string;
 }
